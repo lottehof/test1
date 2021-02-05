316cb4eb58ae3a8a62dbe7653258f2e369986a0b
lottehof <s1112052@student.hsleiden.nl>
2021-02-05 14:57:32 +0100

    test

---
+++ components/Header.vue
@@ -0,0 +1,47 @@
+<template lang="html">
+  <header class="header">
+    <ul class="nav-items">
+      <li>
+        <Nuxt-link to="/">Home</Nuxt-link>
+      </li>
+        <li>
+          <nuxt-link to="/Users">Users</nuxt-link>
+        </li>
+
+    </ul>
+  </header>
+
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+.header{
+  width: 100%;
+  height: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #ccc;
+}
+.nav-items{
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 80%;
+}
+.nav-items a{
+  text-decoration: none;
+  color: black;
+}
+.nav-items a:hover,
+.nav-items a:active{
+  color: white;
+}
+</style>
---
+++ layouts/error.vue
@@ -0,0 +1,8 @@
+<template lang="html">
+  <h1>an error occured, we're sorry!</h1>
+</template>
+
+<script>
+export default {
+}
+</script>
